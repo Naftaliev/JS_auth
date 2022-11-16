937034f13248895b4c2ab6430d562d0042fb59cf
Ilya <n2i1a95@yandex.ru>
2022-11-16 22:00:58 +0300

    input validation

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,32 @@
+import {isValid} from "./utils.js";
+
 const form = document.getElementById("form");
-const inpit = form.querySelector("#question_input");
-const submitBtn = form.querySelector("#question_input");
+const input = form.querySelector("#question_input");
+const submitBtn = form.querySelector("#submit");
 
+form.addEventListener("submit", submitFormHandler);
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value)
+})
 
+const submitFormHandler = (event) => {
+    event.preventDefault()
+
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+
+        submitBtn.disabled = true // disable button to avoid request spamming
+
+        // Async server request , saving question
+        console.log('Question', question)
+
+        input.value = '';
+        input.className = '';
+
+        
+    }
+
+}
