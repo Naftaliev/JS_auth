258f2b8f335f8a06d3b1cf54018881e7a398dcdf
Ilya <n2i1a95@yandex.ru>
2022-11-21 19:52:41 +0300

    entries saved to localStorage

--- src/js/question.js
+++ src/js/question.js
@@ -5,12 +5,26 @@ export class Question {
             method: 'POST',
             body: JSON.stringify(question), // понятный для firebase формат
             headers: {
-                'Content-Type': 'applicatio2n/json'
+                'Content-Type': 'applicatio2n/json' //дефис – не валидный символ для ключа, поэтому в кавычках
             }
         })
             .then(response => response.json())
             .then(response => {
-                console.log(response)
+                question.id = response.name; //имя в firebase вместо криптоключа
+                return question
             })
+            .then(addToLocalStorage)
     }
+}
+
+function addToLocalStorage(question) {
+    const all = getQuestionsfromLS()
+    all.push(question)
+    /* localStorage.setItem('questions', JSON.stringify(question)) */ //при таком способе новые данные заменяют старый объект в хранилище
+    localStorage.setItem('questions', JSON.stringify(all))
+
+}
+
+function getQuestionsfromLS() {
+    return JSON.parse(localStorage.getItem('questions') || '[]')
 }
